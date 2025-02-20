5bd25868864372038265d27a0ea8be5615da8deb
Himansh <himansh402.be22@chitkara.edu.in>
2025-02-20 13:10:25 +0530

    ShopVista added

---
+++ client/src/redux/store.js
@@ -0,0 +1,18 @@
+
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import cartReducer from "./cartRedux";
+import userReducer from "./userSlice"
+
+
+export default configureStore({
+    reducer:{
+        cart:cartReducer,
+        user:userReducer,
+    },
+})
+
+
+
+
+
+
